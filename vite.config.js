523864d259add17d3d21e4f46a7ec4d326fc7962
Elliott Miller <elliotttmiller@hotmail.com>
2026-09-05 04:11:46 -0500

    UPDATE

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'docs',
     target: 'es2020',
     sourcemap: false,
     cssCodeSplit: true,
