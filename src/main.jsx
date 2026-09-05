96e9df47f2dd8bb05cea5400dde14368601017d4
Elliott Miller <elliotttmiller@hotmail.com>
2026-09-04 23:27:03 -0500

    Remove runtime asset bridge initialization

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { installAssetBridge } from './assetBridge'
 import './styles.css'
-
-installAssetBridge()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
