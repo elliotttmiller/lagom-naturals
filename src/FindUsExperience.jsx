15c633458f4171149a00874f272b69516a230625
Elliott Miller <elliotttmiller@hotmail.com>
2026-09-08 16:56:57 -0500

    Use dedicated mobile storefront image on Find Us

--- src/FindUsExperience.jsx
+++ src/FindUsExperience.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
-import storefrontMobile from '@/assets/mobile/main-store.png'
+import storefrontMobile from '@/assets/mobile/storefront.png'
 
 const SHOPS=[
 ['Barstock Liquors','31 E Main St','Crosby','MN','56441','+12185453004','http://www.barstockliquors.com/'],
